b65d9b1366ad9d59c1b1343806a1951e5f09204d
Apip Kurniawan <davitaja92@gmail.com>
2024-07-27 12:53:43 +0700

    feat : create state pattern

---
+++ state/index.ts
@@ -0,0 +1,60 @@
+namespace State {
+  interface IProduct {
+    name: string;
+    saveAsFeaturedProduct(): void;
+  }
+
+  class FeaturedProduct {
+    private state: IProduct;
+
+    constructor(state: IProduct) {
+      this.state = state;
+    }
+
+    setFeaturedProduct(state: IProduct): void {
+      this.state = state;
+    }
+
+    getFeaturedProduct(): void {
+      console.log("state...", this.state);
+    }
+
+    publish(): void {
+      this.state.saveAsFeaturedProduct();
+    }
+  }
+
+  class FashionProduct implements IProduct {
+    name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
+
+    saveAsFeaturedProduct(): void {
+      console.log("set product " + this.name + " sebagai product unggulan");
+    }
+  }
+
+  class ElectronicProduct implements IProduct {
+    name: string;
+    constructor(name: string) {
+      this.name = name;
+    }
+
+    saveAsFeaturedProduct(): void {
+      console.log("set product " + this.name + " sebagai product unggulan");
+    }
+  }
+
+  const baju = new FashionProduct("baju");
+  const celana = new FashionProduct("celana");
+  const keyboard = new ElectronicProduct("keyboard");
+
+  const featuredProduct = new FeaturedProduct(baju);
+  featuredProduct.publish();
+  featuredProduct.getFeaturedProduct();
+
+  featuredProduct.setFeaturedProduct(keyboard);
+  featuredProduct.publish();
+  featuredProduct.getFeaturedProduct();
+}
